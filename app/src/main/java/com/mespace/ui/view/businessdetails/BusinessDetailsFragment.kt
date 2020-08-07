43eaa249ad0a9dbe35cf89b6a02437b3c181d9fe
bharath <bharath@ndot.in>
2020-08-07 10:10:03 +0530

    SettingsFragment, EditSpaceFragment, SearchFragment,MySpaceBottomFragment, BusinessDetailsFragment.kt

--- app/src/main/java/com/mespace/ui/view/businessdetails/BusinessDetailsFragment.kt
+++ app/src/main/java/com/mespace/ui/view/businessdetails/BusinessDetailsFragment.kt
@@ -38,6 +38,7 @@ class BusinessDetailsFragment : Fragment(), LifecycleObserver {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         lifecycle.addObserver(this)
     }
 
